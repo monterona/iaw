f8c441fd5c80fef72d34c5436c4baadd1ba0879d
Joaquinma <monterona@gmail.com>
2022-02-17 07:26:36 +0100

    Formulario

---
+++ formularioprocesa.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulario procesa información</title>
+</head>
+<body>
+	<?php
+		$usuario = $_GET['usuario'];
+		$departamento = $_GET['departamento'];
+		echo "El usuario es: " . $usuario;
+		echo "<br>";
+		print ("El departamento es: $departamento");
+	?>
+</body>
+</html>
---
+++ formulariousuario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulario usuario</title>
+</head>
+<body>
+		<form action="formularioprocesa.php" method="get">
+			Usuario: <input type="text" name="usuario"/>
+			Departamento: <input type="text" name="departamento"/>
+			<input type="submit" value ="Enviar"/>
+		</form>
+</body>
+</html>
